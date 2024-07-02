ec8ed3fa3f89b281999aef5dd7dbb43511e4f4f5
Rafiul29 <rafiulislam4472@gmail.com>
2024-07-02 18:16:49 +0600

    appointment mode create

--- appointment/admin.py
+++ appointment/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Appointment
 # Register your models here.
+class AppointmentAdmin(admin.ModelAdmin):
+  list_display=['patient','doctor','appointment_types','appointment_status','symptom','time','cancle']
+
+  def doctor(self,obj):
+    return self.doctor.user.first_name
+  
+
+  def patient(self,obj):
+    return self.patient.user.first_name
+
+admin.site.register(Appointment,AppointmentAdmin)
