1af05a402d7783a23f1339c59ea1170ca630634e
Rafiul29 <rafiulislam4472@gmail.com>
2024-07-03 00:26:59 +0600

    doctor api done

--- doctor/models.py
+++ doctor/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from patient.models import Patient
+
 # Create your models here.
 class Specialization(models.Model):
   name=models.CharField(max_length=50)
--- doctor/views.py
+++ doctor/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Doctor,Specialization,Designation,AvailableTime,Review
 
+from .serializers import DoctorSerializer,SpecializationSerializer,DesignationSerializer,AvailableTimeSerializer,ReviewSerializer
 # Create your views here.
+
+class SpecializationViewSet(viewsets.ModelViewSet):
+  queryset=Specialization.objects.all()
+  serializer_class=SpecializationSerializer
+
+class DesignationViewSet(viewsets.ModelViewSet):
+  queryset=Designation.objects.all()
+  serializer_class=DesignationSerializer
+
+class AvailableTimeViewSet(viewsets.ModelViewSet):
+  queryset=AvailableTime.objects.all()
+  serializer_class=AvailableTimeSerializer
+
+class DoctorViewSet(viewsets.ModelViewSet):
+  queryset=Doctor.objects.all()
+  serializer_class=DoctorSerializer
+
+class ReviewViewSet(viewsets.ModelViewSet):
+  queryset=Review.objects.all()
+  serializer_class=ReviewSerializer
