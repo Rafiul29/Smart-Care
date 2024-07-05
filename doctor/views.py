e3b5177ad4efc4a316469323ffdd26545a228d09
Rafiul29 <rafiulislam4472@gmail.com>
2024-07-05 21:19:02 +0600

    doctor pagination added and doctor specific available time  search added

--- doctor/views.py
+++ doctor/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets,filters,pagination
+
 from .models import Doctor,Specialization,Designation,AvailableTime,Review
 
 from .serializers import DoctorSerializer,SpecializationSerializer,DesignationSerializer,AvailableTimeSerializer,ReviewSerializer
@@ -9,6 +10,8 @@ from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly
 
 # Create your views here.
 
+
+
 class SpecializationViewSet(viewsets.ModelViewSet):
   queryset=Specialization.objects.all()
   serializer_class=SpecializationSerializer
@@ -17,14 +20,32 @@ class DesignationViewSet(viewsets.ModelViewSet):
   queryset=Designation.objects.all()
   serializer_class=DesignationSerializer
 
+
+class AvailableTimeForSpecificDoctor(filters.BaseFilterBackend):
+  def filter_queryset(self,request,query_set,view):
+    doctor_id=request.query_params.get('doctor_id')
+    if doctor_id:
+      return query_set.filter(doctor=doctor_id)
+    return query_set
+
+
 class AvailableTimeViewSet(viewsets.ModelViewSet):
 
   queryset=AvailableTime.objects.all()
   serializer_class=AvailableTimeSerializer
+  filter_backends=[AvailableTimeForSpecificDoctor]
+
+class DoctorPagination(pagination.PageNumberPagination):
+  page_size=1
+  page_size_query_param=page_size
+  max_page_size=50
 
 class DoctorViewSet(viewsets.ModelViewSet):
   queryset=Doctor.objects.all()
   serializer_class=DoctorSerializer
+  filter_backends=[filters.SearchFilter]
+  pagination_class=DoctorPagination
+  search_fields = ['user__first_name', 'user__email', 'designation__name', 'specialization__name']
 
 class ReviewViewSet(viewsets.ModelViewSet):
   permission_classes=[IsAuthenticated]
